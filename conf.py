b27f24c3b493ef015055ae4778fbf5dc2f2283a0
DaniyarNaamatov <100943927+DaniyarNaamatov@users.noreply.github.com>
2022-04-27 13:42:05 +0600

    GTX

---
+++ conf.py
@@ -0,0 +1,3 @@
+URL = 'https://svetofor.info/noutbuki-planshety-bukridery/'
+TOKEN = '5374935637:AAETLrTsF9UPQyFMrcEXOgG5ccf7TPBm08U'
+DRIVER_PATH = 'C://Users//user//PycharmProjects//trade_parser//chromedriver.exe'
---
+++ config.py
@@ -0,0 +1,3 @@
+URL = 'https://www.computeruniverse.net/ru/c/apparatnoe-obespechenie-i-komponenty/videokarty-pci-express?page={}'
+TOKEN = '5374935637:AAETLrTsF9UPQyFMrcEXOgG5ccf7TPBm08U'
+DRIVER_PATH = 'C://Users//user//PycharmProjects//trade_parser//chromedriver.exe'
